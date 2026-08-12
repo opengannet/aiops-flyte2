28d0c4da6ee96bb3817bc005c01159835a2fe626
kevin <kevinj@gmail.com>
2026-08-12 14:14:25 +0800

    fix(console): restore Scalar API docs interactions

--- flyte_console/src/components/AioneApiDocs/ScalarApiReference.tsx
+++ flyte_console/src/components/AioneApiDocs/ScalarApiReference.tsx
@@ -2,6 +2,7 @@
 
 import { ApiReferenceReact } from "@scalar/api-reference-react";
 import "@scalar/api-reference-react/style.css";
+import "./scalar-overrides.css";
 
 export const aioneOpenApiUrl = "/v2/openapi/aione.yaml";
 
